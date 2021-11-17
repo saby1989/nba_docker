3851380daa1444e837b627ac90fddf7642db4f1a
saby1989 <kim.saba@yahoo.com>
2021-11-17 17:40:53 +0530

    Version 0
    dummy veriosn created

---
+++ index.py
@@ -0,0 +1,14 @@
+#import numpy as np
+#import pandas as pd
+from datetime import datetime
+
+if __name__ =='__main__':
+     '''
+     print("initializing a variable")
+     a = np.arange(1,20,1)
+     a = pd.DataFrame(a, columns=['pers_id'])
+
+     print(f" the shape of the dataframe is {a.shape}")
+     #a.to_csv('output.csv',index=False)
+     '''
+     print(f'the time of run is {datetime.now()}')
---
+++ userid.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Display the UID and username of the user execurting the script
+#Display if the user is root or not
+
+#get the userid and username
+USERNAME=$(id -n -u)
+USERID=${UID}
+
+#Display the USERID
+echo "the current userid is ${USERID} and username is $USERNAME"
+
+#running the python file
+#pip3 install -r /app/requirements.txt
+python3 /app/index.py
+
+if [[ ${?} -eq 0 ]]
+then
+  echo  "the python code has completed sucessfully"
+else
+  echo "the python code has failed"
+fi
